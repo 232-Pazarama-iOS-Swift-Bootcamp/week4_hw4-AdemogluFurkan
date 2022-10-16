590e9e8b518a55ba997b456cf76b335e30c35ea9
FurkanAdemoglu <furkanademoglu23@gmail.com>
2022-10-16 12:51:41 +0300

    tab bar is coded

---
+++ flickrApp/flickrApp/Screens/TabBar/Controller/MainTabbarViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MainTabbarViewController.swift
+//  flickrApp
+//
+//  Created by Furkan Ademoğlu on 15.10.2022.
+//
+
+import UIKit
+
+class MainTabbarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let vc1 = UINavigationController(rootViewController: PhotoListViewController(viewModel: PhotoListViewModel()))
+        let vc2 = UINavigationController(rootViewController: SearchViewController(viewModel: SearchViewModel()))
+                       
+             //  vc1.tabBarItem.image = UIImage(systemName: "ipod")
+
+               
+        vc1.title = "Home"
+        vc2.title = "Search"
+               
+               
+        tabBar.tintColor = .label
+        setViewControllers([vc1,vc2], animated: true)
+    }
+}
